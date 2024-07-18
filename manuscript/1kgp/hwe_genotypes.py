9c6b6108b96dd080f65fd7c11c4048b1b19c3de9
Adam English <ACEnglish@gmail.com>
2024-07-18 09:19:02 -0500

    adding sizelimits to hwe gts

--- manuscript/1kgp/hwe_genotypes.py
+++ manuscript/1kgp/hwe_genotypes.py
@@ -2,16 +2,22 @@ import sys
 import pysam
 import joblib
 import numpy as np
-
+import truvari
+SZMIN = 50
+SZMAX = 10000
 def parse_vcf(vcf_file):
     vcf = pysam.VariantFile(vcf_file)
     genotype_counts = []
-    for record in vcf:
-        if record.chrom == 'chrX' or record.chrom == 'chrY':
+    for entry in vcf:
+        if entry.chrom == 'chrX' or entry.chrom == 'chrY':
             continue
+        sz = truvari.entry_size(entry)
+        if sz < SZMIN or sz > SZMAX:
+            continue
+
         hom_ref, het, hom_alt, missing = np.uint16(), np.uint16(), np.uint16(), np.uint16()
 
-        for sample in record.samples.values():
+        for sample in entry.samples.values():
             if None in sample['GT']:
                 missing  += 1
                 continue
@@ -22,6 +28,7 @@ def parse_vcf(vcf_file):
                 het += 1
             elif ac == 2:
                 hom_alt += 1
+        
         genotype_counts.append((hom_ref, het, hom_alt, missing))
     return np.array(genotype_counts, dtype=np.uint16)
 
@@ -29,3 +36,4 @@ if __name__ == '__main__':
     in_vcf = sys.argv[1]
     out_jl = sys.argv[2]
     joblib.dump(parse_vcf(in_vcf), out_jl)
+    print('finished', in_vcf)
