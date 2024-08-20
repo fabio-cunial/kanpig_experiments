295628afcf54a58dfc6de224b16d69aecf2c0edb
fcunial <fcunial@broadinstitute.org>
2024-08-20 14:54:32 -0400

    cosmetic

--- docker/pushDocker.sh
+++ docker/pushDocker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
 TAG=""
+cp ../manuscript/full_analysis/analysis.py .
 docker build --progress=plain -t fcunial/kanpig_experiments .
 docker push fcunial/kanpig_experiments${TAG}
+rm -f analysis.py
